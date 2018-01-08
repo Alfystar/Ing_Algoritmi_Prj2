e2d624780cdbc6aac7e47cab6425300a395973ac
filippo <filibad25051997@gmail.com>
2018-01-08 13:56:18 +0100

    cartelle sistemate

--- makeGraph/creaGrafo.py
+++ makeGraph/creaGrafo.py
@@ -1,9 +1,11 @@
 import random
-from graph.graph.Graph_AdjacencyList import *
-from graph.priorityQueue.PQbinaryHeap import *
+
 
 def mkGraph(type, elem):
     newGr = GraphAdjacencyList()
     newGr.addNode(random.randint(-10,10))   #nodo con valore casuale
 
     return newGr
+
+if __name__ == '__main__':
+    print(50)
