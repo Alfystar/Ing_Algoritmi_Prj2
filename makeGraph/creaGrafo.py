609dd708321b7b13a85c0cdf63a3956274b8c2e1
alfylinux <emanuele.alfano@alice.it>
2018-01-08 15:13:02 +0100

    crea grafo random

--- makeGraph/creaGrafo.py
+++ makeGraph/creaGrafo.py
@@ -5,20 +5,52 @@ from graph.Graph_AdjacencyList import *
 from stack.Stack import PilaArrayList as stack
 
 
-def mkGraph(mod, elem):
+def mkGraph(mod, elem, son):
     """
-    :param mod: modalita: 0-> random
-    :param elem: numero massimodi elementi da aggiungere
+    :param mod: modalita: "rand",
+    :param elem: numero massimo di elementi da aggiungere
+    :param son: numero di figli, nel caso random il massimo possibile
     :return: grafico sotto forma di lista di adiacenza
     """
     newGr = GraphAdjacencyList()
     firstNode = newGr.addNode(rInt(-10, 10))  # nodo con valore casuale
     sk = stack()
-    sk.push(firstNode.id)
+    sk.push(firstNode)
     sk.stampa()
-    for i in range(elem):
+    if mod=="rand":
+        return randomGrap(newGr,sk,elem,son)
 
-    return newGr
+
+def randomGrap(G,pila,elemMax,sonMax):
+    """
+    :param G: grafo
+    :param pila:
+    :param elemMax:
+    :param sonMax:
+    :return:
+    """
+    count = 0
+    while (count <= elemMax):
+        node = pila.pop()  # prendo nodo già inserito
+        for k in range(rInt(0, sonMax)):  # gli aggiungo un numero casuale di figli
+            sonMax = G.addNode(rInt(-10, 10))
+            notOriented(newGr, node, sonMax)
+            pila.push(sonMax)
+            count += 1  # tengo traccia dell'aumento dei nodi
+    del pila  # cancello oggetto per far sovrascrivere memoria, la pila non è più necessaria
+    return G
+
+
+
+def notOriented(g,n1,n2):
+    """
+    :param g: grafo
+    :param n1: nodo 1
+    :param n2: nodo 2
+    :return: null
+    """
+    g.insertEdge(n1.id, n2.id)
+    g.insertEdge(n2.id, n1.id)
 
 
 if __name__ == '__main__':
