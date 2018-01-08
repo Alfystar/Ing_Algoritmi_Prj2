47cd1450eb9fb00bff70d508615c93edf63da8cc
filippo <filibad25051997@gmail.com>
2018-01-08 19:42:43 +0100

    forme grafo (migliorare alcune funzioni, piccoli ritocchi e commenti)

--- makeGraph/creaGrafo.py
+++ makeGraph/creaGrafo.py
@@ -3,9 +3,10 @@ from random import randint as rInt
 from graph.Graph_AdjacencyList import *
 # from priorityQueue.PQbinomialHeap import PQbinomialHeap
 from stack.Stack import PilaArrayList as stack
+from queue.Queue import CodaArrayList_deque as queue
 
 
-def mkGraph(elem, son, mod="rand"):
+def mkGraph(elem, mod="rand", son = None):
     """
     :param mod: modalita: "rand", "star", "linear", "fractal"
     :param elem: numero massimo di elementi da aggiungere
@@ -14,22 +15,23 @@ def mkGraph(elem, son, mod="rand"):
     """
     newGr = GraphAdjacencyList()
     firstNode = newGr.addNode(rInt(-10, 10))  # nodo con valore casuale
-    sk = stack()
-    sk.push(firstNode)
+
     if mod == "rand":
-        randomGraph(newGr, sk, elem, son)
+        randomGraph(newGr, firstNode, elem, son)
 
     elif mod == "star":
-        starGraph(newGr, sk, elem)
+        starGraph(newGr, firstNode, elem)
 
     elif mod == "linear":
-        linearGraph(newGr, sk, elem)
+        linearGraph(newGr, firstNode, elem)
 
-    del sk  # eliminiamo la pila modificata in uno dei metodi
+    elif mod == "fractal":
+        fractalGraph(newGr, firstNode, elem, son)
+
     return newGr
 
 
-def randomGraph(G, pila, elemLimit, sonLimit):
+def randomGraph(G, fN, elemLimit, sonLimit):
     """
     :param G: grafo
     :param pila:
@@ -37,30 +39,40 @@ def randomGraph(G, pila, elemLimit, sonLimit):
     :param sonLimit:
     :return:
     """
+    pila = stack()
+    pila.push(fN)
+
     count = 1
     while (count <= elemLimit-1):
-        if (not pila.isEmpty()): node = pila.pop()  # prendo nodo già inserito
+        if (not pila.isEmpty()):
+            node = pila.pop()  # prendo nodo già inserito
         for k in range(rInt(0, sonLimit)):  # gli aggiungo un numero casuale di figli
+            if (count > elemLimit-1):
+                break
             son = G.addNode(rInt(-10, 10))
             notOriented(G, node, son)
             pila.push(son)
             count += 1  # tengo traccia dell'aumento dei nodi
+    del pila  # eliminiamo la pila
 
-
-def starGraph(G, pila, elemLimit):
+def starGraph(G, fN, elemLimit):
     """
     :param G:
     :param pila:
     :param elemLimit:
     :return:
     """
+
+    pila = stack()
+    pila.push(fN)
     node = pila.pop()
+
     for k in range(elemLimit-1):
         son = G.addNode(rInt(-10, 10))
         notOriented(G, node, son)
+    del pila # eliminiamo la pila
 
-
-def linearGraph(G, pila, elemLimit):
+def linearGraph(G, fN, elemLimit):
     """
 
     :param G:
@@ -68,6 +80,8 @@ def linearGraph(G, pila, elemLimit):
     :param elemLimit:
     :return:
     """
+    pila = stack()
+    pila.push(fN)
     count = 1
     while (count <= elemLimit-1):
         node = pila.pop()  # prendo nodo già inserito
@@ -75,6 +89,33 @@ def linearGraph(G, pila, elemLimit):
         notOriented(G, node, son)
         pila.push(son)
         count += 1  # tengo traccia dell'aumento dei nodi
+    del pila  # eliminiamo la pila
+
+def fractalGraph(G, fN, elemLimit, sonLimit):
+    """
+    :param G: grafo
+    :param pila:
+    :param elemLimit:
+    :param sonLimit:
+    :return:
+    """
+    coda = queue()
+    coda.enqueue(fN)
+
+    count = 1
+    while (count <= elemLimit-1) and (elemLimit-count >= sonLimit):
+        if (not coda.isEmpty()):
+            node = coda.dequeue()  # prendo nodo già inserito
+        for k in range(sonLimit):  # gli aggiungo un numero definito di figli
+            if (count > elemLimit - 1):
+                break
+            son = G.addNode(rInt(-10, 10))
+            notOriented(G, node, son)
+            coda.enqueue(son)
+            count += 1  # tengo traccia dell'aumento dei nodi
+
+    del coda  # eliminiamo la coda
+
 
 def notOriented(g, n1, n2):
     """
@@ -88,11 +129,14 @@ def notOriented(g, n1, n2):
 
 
 if __name__ == '__main__':
-    i = mkGraph(10, 2, "rand")
-    i.print()
+    g1 = mkGraph(20, "rand", 5)
+    g1.print()
     print('')
-    j = mkGraph(10, 2, "star")
-    j.print()
+    g2 = mkGraph(20, "star")
+    g2.print()
     print('')
-    k = mkGraph(10, 2, "linear")
-    k.print()
+    g3 = mkGraph(20, "linear")
+    g3.print()
+    print('')
+    g4 = mkGraph(22, "fractal", 3)
+    g4.print()
