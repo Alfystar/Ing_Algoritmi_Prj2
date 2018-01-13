6aa84ec9a0198856155a5be4c7879616a886232f
alfylinux <emanuele.alfano@alice.it>
2018-01-13 12:04:38 +0100

    matlab Plot

--- matlab SCRIPT/plotTime.m
+++ matlab SCRIPT/plotTime.m
@@ -1,12 +1,5 @@
 clear;
 close all;
-%Predispongo il plot dei dati
-hold on
-grid on
-xlabel("Numero di elementi")
-ylabel("Time to find MED")
-legend('Location','northwest')
-
 
 %%%Carico le varie tabelle
 filename = 'starTime.dat';
@@ -14,7 +7,7 @@ T = readtable(filename);
 star= table2array(T);
 
 
-filename = 'fractalTime.dat';
+filename = 'fractalSon2_Time.dat';
 T = readtable(filename);
 fractal= table2array(T);
 
@@ -23,7 +16,37 @@ filename = 'linearTime.dat';
 T = readtable(filename);
 linear= table2array(T);
 
+filename = 'rand2Son100_Time.dat';
+T = readtable(filename);
+sfilacciato= table2array(T);
 
+filename = 'randTime.dat';
+T = readtable(filename);
+random= table2array(T);
+
+
+
+subplot(1,2,1);
 % creo il grafico concatenando le varie tabelle
-plot(star(:,1),star(:,2),'g',fractal(:,1),fractal(:,2),'r',linear(:,1),linear(:,2))
-legend('stella','Frattale','lineare')
+plot(star(:,1),star(:,2),'g',fractal(:,1),fractal(:,2),'r',linear(:,1),linear(:,2),'b',sfilacciato(:,1),sfilacciato(:,2),'c',random(:,1),random(:,2),'k')
+legend('Stella','Frattale','Lineare','Sfilacciato','Random')
+%Predispongo il plot dei dato
+hold on
+grid on
+xlabel("N° elem")
+ylabel("Time to find MED")
+legend('Location','northwest')
+title('Tutte le funzioni')
+
+
+subplot(1,2,2);
+% creo il grafico concatenando le varie tabelle
+semilogy(star(:,1),star(:,2),'g',fractal(:,1),fractal(:,2),'r',linear(:,1),linear(:,2),'b',sfilacciato(:,1),sfilacciato(:,2),'c',random(:,1),random(:,2),'k')
+legend('Stella','Frattale','Lineare','Sfilacciato','Random')
+%Predispongo il plot dei dato
+hold on
+grid on
+xlabel("N° elem")
+ylabel("Time to find MED")
+legend('Location','northwest')
+title('Confronto Logaritmico delle stesse')
