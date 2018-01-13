60fa8728145f5f5be010901e48dbfd791640eb15
alfylinux <emanuele.alfano@alice.it>
2018-01-13 11:13:27 +0100

    Bash runner, if per ridurrre errori

--- runAndData.sh
+++ runAndData.sh
@@ -1,29 +1,41 @@
-﻿#!/bin/bash
+#!/bin/bash
 #argomeni da passare:
 # arg1= linear,star,fractal,rand2,rand
 # arg2= 1/0 (output medio)
 # arg3= nElem
 # arg4= OutFileDir
 # arg5= son (se la modalità la prevede ma non chiama errore)
+# arg6= dove piazzare i file del cProfile
 
-echo creo la cartella $4
-mkdir $4	#creo la cartella dove verranno salvati i OutFileDir
-mkdir $4/cProfile	#creo la cartella dove verranno salvati i OutFileDir
 
-echo  "parametri passati: ""nElem=" $3 " sonNumber="$5 " OutDir=" $4" OuputData=" $2 " Modalità=" $1;
+if [[ -d $4 ]];then
+	echo già esistente $4
+else
+	echo creo la cartella $4
+	mkdir $4	#creo la cartella dove verranno salvati i OutFileDir
+fi
+
+if [[ -d $4/cProfile ]];then
+	echo "già esistente ./cProfile"
+else
+	echo "creo ./cProfile"
+	mkdir $4/cProfile	#creo la cartella dove verranno salvati i OutFileDir
+fi
+
+echo  "parametri passati: ""nElem=" $3 " sonNumber="$5 " OutDir=" $4" OuputData=" $2 " Modalità=" $1 "Dati in uscita su:" $6;
 #python3 -m cProfile -o <output cProfile> demoMain.py <modalità> <OutputResul> <nElement> <son>
 
-#[["$1" != "fractal"] || ["$1" !="rand2"]]
-#if [[$1 != "fractal"] && [$1 !="rand2"]];	then
-#		out="son$4Time.dat"
-#		echo è frattale o sfilacciato
-#	else
-#		out="Time.dat"
-#		echo non è frattale o sfilacciato
-#fi
-out="son$4Time.dat"
-mkdir $4/cProfile/"$1x$3elemx$5son"
+#!!! Modifico la variabile Out al fine di rendere più leggibile la funzione
+out=$6
+
 dirProf="$1x$3elemx$5son"
-python3 -m cProfile -o $4/cProfile/$dirProf/"$1x$3x$5"elemTest.o demoMain.py $1 $2 $3 $5 >>$4/"$1$out"	#creo tanti cProfile diversi, ma 1 soo file con i tempi
+if [[ -d $4/cProfile/$dirProf ]];then
+	echo già esistente
+else
+	echo creo la cartella $dirProf
+	mkdir $4/cProfile/$dirProf
+fi
+
+python3 -m cProfile -o $4/cProfile/$dirProf/"$1x$3x$5"elemTest.o demoMain.py $1 $2 $3 $5 >> $4/"$1$out"	#creo tanti cProfile diversi, ma 1 soo file con i tempi
 echo progamma eseguito
-python3 profileShow.py $4/cProfile/$dirProf/"$1x$3x$5"elemTest.o cumtime >$4/cProfile/$dirProf/"$1x$3x$5"elemTest.dat
+python3 profileShow.py $4/cProfile/$dirProf/"$1x$3x$5"elemTest.o cumtime > $4/cProfile/$dirProf/"$1x$3x$5"elemTest.dat
--- serialRun.sh
+++ serialRun.sh
@@ -1,32 +1,31 @@
-﻿#!/bin/bash
+#!/bin/bash
 #argomeni da passare:
 # arg1= linear,star,fractal,rand2,rand
 # arg2= quante volte aumento di 200 il numero di elementi partendo da 200
 # arg3= OutFileDir
 # arg4= son (se la modalità la prevede ma non chiama errore)
 
-echo eseguo lo script:
-#./runAndData.sh  <forma> <output del medio> <nElem> <OutFileDir> <son>
-
-#./runAndData.sh  $1 0 $2 $3 $4
-
-#[["$1" != "fractal"] || ["$1" !="rand2"]]
-#if [[$1 != "fractal"] && [$1 !="rand2"]];	then
-#		out="son$4Time.dat"
-#		echo è frattale o sfilacciato
-#	else
-#		out="Time.dat"
-#		echo non è frattale o sfilacciato
-#fi
+echo eseguo lo script Serial Run per la forma $1, fino a $(($2*200)) Elementi.
+#./runAndData.sh  <forma> <output del medio> <nElem> <OutFileDir> <son> <outDir>
+#MEMO  ./runAndData.sh  $1 0 $2 $3 $4
 
 
-out="son$4Time.dat"
+#!!! Modifico la variabile Out al fine di rendere più leggibile la funzione
+if ( [ $1 != "fractal" ] && [ $1 != "rand2" ] );
+then
+	out="Time.dat"
+	echo non è frattale o sfilacciato
+else
+	out="Son$4_Time.dat"
+	echo è frattale o sfilacciato
+fi
+
 echo Modalità "$1" > $3/"$1$out" 	#svuoto il file che conterrà i tempi
 echo "nElem, Time(s)" >> $3/"$1$out"  	#creo nomi colonne
 echo "<#>./serialRun $1 $2 $3 $4">>$3/logAction.log
 for i in $(seq $2);
 do
-	./runAndData.sh  $1 0 $((200*$i)) $3 $4
+	./runAndData.sh  $1 0 $((200*$i)) $3 $4 $out
 	echo fatto "$i"
 	echo $(date) "iterazione $i" $'\n\t' "./runAndData.sh  $1 0 $((200*$i)) $3 $4">>$3/logAction.log
 
